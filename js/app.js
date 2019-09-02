3e305455e8b244f986c0145f3fa371afe8a9f2b7
zwy <zhaowenjue@thtf.com.cn>
2019-09-02 17:25:18 +0800

    add project

---
+++ js/app.js
@@ -0,0 +1,53 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#problems").html("");
+}
+
+function connect() {
+    var socket = new SockJS('http://localhost:10014/my-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/test', function (problem) {
+            console.debug('problem - ', problem);
+            showProblem(problem.body);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendAlarm() {
+    stompClient.send("/welcome/heihei", {}, JSON.stringify({'content': $("#content").val()}));
+}
+
+function showProblem(message) {
+    $("#problems").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+    $( "#send" ).click(function() { sendAlarm(); });
+});
+
+
